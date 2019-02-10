ddf32a4e17ff0afd134d69c962b413dd71988481
Robert Lee <bobbydwise1@hotmail.com>
2019-02-09 18:02:10 -0800

    Right move works for single block but not multi-block.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,12 +21,12 @@ function Game(points,level) {
   this.hitDetectLookAhead3 = this.zeroMatrix3;
   this.hitDetectLookAhead4 = this.zeroMatrix4;
   this.system = [
-    [2,0,0,0,0,0,0,0,0,2],
-    [3,0,0,0,0,0,0,0,0,3],  //system[0] hidden top
-    [4,0,0,0,0,0,0,0,0,4],
-    [5,0,0,0,0,0,0,0,0,5],
-    [6,0,0,0,0,0,0,0,0,6],
-    [7,0,0,0,0,0,0,0,0,7],
+    [-2,0,0,0,0,0,0,0,0,-2],
+    [-3,0,0,0,0,0,0,0,0,-3],  //system[0] hidden top
+    [-4,0,0,0,0,0,0,0,0,-4],
+    [-5,0,0,0,0,0,0,0,0,-5],
+    [-6,0,0,0,0,0,0,0,0,-6],
+    [-7,0,0,0,0,0,0,0,0,-7],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -34,14 +34,14 @@ function Game(points,level) {
     [0,0,0,0,0,0,0,0,0,0],  //system[10] === MIDDLE!
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
-    [6,0,0,0,0,0,0,0,0,0],
-    [6,0,0,0,0,0,0,0,0,0],
-    [6,6,0,0,0,0,0,0,0,0],
-    [5,5,0,0,0,0,0,0,0,0],
-    [5,0,0,0,0,0,0,0,0,0],
-    [5,0,0,0,0,0,0,0,0,0],
-    [6,5,4,3,3,2,2,1,0,0],
-    [1,2,3,4,5,6,7,8,0,0]  //system[20] === y-axis[20] == bottom
+    [-6,0,0,0,0,0,0,0,0,0],
+    [-6,0,0,0,0,0,0,0,0,0],
+    [-6,-6,0,0,0,0,0,0,0,0],
+    [-5,-5,0,0,0,0,0,0,0,0],
+    [-5,0,0,0,0,0,0,0,0,0],
+    [-5,0,0,0,0,0,-7,0,0,0],
+    [-6,-5,-4,-3,-3,-2,-2,-1,0,0],
+    [-1,-2,-3,-4,-5,-6,-7,-8,0,0]  //system[20] === y-axis[20] == bottom
     ];
 
   this.ooh = [
@@ -94,6 +94,13 @@ function Game(points,level) {
   [0,1,1]
   ];
 
+  this.one =
+  [
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+  ];
+
 Game.prototype.resetSystem = function() {
   for (let y = 0; y < this.system.length; y++) {
     for (let x = 0; x < this.system[0].length; x++) {
@@ -101,6 +108,11 @@ Game.prototype.resetSystem = function() {
     }
   }
   console.log("Reset this.system");
+  this.alive = 0;
+  this.alivePos = [0,0];
+  this.points = 0;
+  this.level = 0;
+  this.lines = 0;
   }
 
 Game.prototype.makePieceDead = function() {
@@ -108,17 +120,28 @@ Game.prototype.makePieceDead = function() {
 }
 
 Game.prototype.clearLines = function() {
-
+  // for (let y = this.system.length-1; y > 0; y--) { //from bottom of system
+  for (let y = 0; y < this.system.length; y++) {
+    if (this.system[y].reduce(function(accumulator,currentValue) {return accumulator + currentValue;}) < -9) {
+      this.system.splice(y, 1);
+      this.system.unshift(this.blankRow);
+      this.lines++;
+    }
+  }
+  this.updateGrid();
 }
 
 //Note the piece insertion coordinate is alaways y=1, x=1;
+//What is the logic attempting?
+//First it determines what is the alive matrix.
+//Second, it needs to "map" the system matrix [y][x] onto the alive matrix
 Game.prototype.insertNewpiece = function(piece_matrix) {
   this.alivePos = [1,4];
   this.alive = piece_matrix;
   for (var y = 1; y < piece_matrix[0].length; y++) {
     for (var x = 0; x < piece_matrix[0].length; x++) {
       if (piece_matrix[y][x] != 0) {
-        this.system[y][3+x] = piece_matrix[y][x];
+        this.system[y][this.alivePos[1]+x-1] = piece_matrix[y][x];
       }
     }
   }
@@ -159,7 +182,6 @@ Game.prototype.rotateCW = function() {
     this.alive = tempArray;
     return this.alive;
   }
-
 
 Game.prototype.rotateCCW = function() {
   if (piece_matrix[0].length === 4) {
@@ -218,7 +240,7 @@ Game.prototype.rotateCCW = function() {
         } else {
           //if your this.alive[y][x] is empty, check the this.system spot to the left of you if it is a dead piece or null.  Since your this.alive[y][x] is empty, you can safely take any this.system value of the spot to the left of you
             if ((this.system[this.alivePos[0]+y-1][this.alivePos[1]+x-2] <= -1) || (this.system[this.alivePos[0]+y-1][this.alivePos[1]+x-2] === null)) {
-            hitDetectLookAhead[y][x] = this.system[this.alivePos[0]+y-1][this.alivePos[1]+x-2];
+            this.hitDetectLookAhead[y][x] = this.system[this.alivePos[0]+y-1][this.alivePos[1]+x-2];
           }
         }
       }
@@ -236,7 +258,7 @@ Game.prototype.rotateCCW = function() {
     }
     for (var y = 0; y < this.alive[0].length; y++) {
       for (var x = this.alive[0].length-1; x >= 0; x--) {
-        console.log("loop no: ", y,"-",x,": this.system scan[y][x]: ",this.alivePos[0]+y-1,"-",this.alivePos[1]+x+rightoffset," value: ",this.system[this.alivePos[0]+y-1][this.alivePos[1]+x+rightoffset])
+        // console.log("loop no: ", y,"-",x,": this.system scan[y][x]: ",this.alivePos[0]+y-1,"-",this.alivePos[1]+x+rightoffset," value: ",this.system[this.alivePos[0]+y-1][this.alivePos[1]+x+rightoffset])
         //if your this.alive[y][x] spot is === 1
         if (this.alive[y][x] === 1) {
           //check if the right box is a dead piece or null
@@ -251,7 +273,7 @@ Game.prototype.rotateCCW = function() {
           //if your this.alive[y][x] is empty, check the this.system spot to the right of you if it is a dead piece or null.  Since your this.alive[y][x] is empty, you can safely take any this.system value of the spot to the left of you
             if ((this.system[this.alivePos[0]+y-1][this.alivePos[1]+x+rightoffset] <= -1) || (this.system[this.alivePos[0]+y-1][this.alivePos[1]+x+rightoffset] === null)) {
             this.hitDetectLookAhead[y][x] = this.system[this.alivePos[0]+y-1][this.alivePos[1]+x+rightoffset];
-            console.log(y,"-",x,": ",this.system[this.alivePos[0]+y-1][this.alivePos[1]+x+rightoffset])
+            // console.log(y,"-",x,": ",this.system[this.alivePos[0]+y-1][this.alivePos[1]+x+rightoffset])
           }
           // debugger;
         }
@@ -262,32 +284,40 @@ Game.prototype.rotateCCW = function() {
   }
 
   Game.prototype.moveLeft = function() {
-    this.alivePos[1]--;
+    // console.log("start alivePos= ",this.alivePos);
     for (var y = 0; y < this.alive[0].length; y++) {
       for (var x = 0; x < this.alive[0].length; x++) {
-        if (this.system[y][x+this.alivePos[1]] === this.alive[y][x]) {
-          this.system[y][x+this.alivePos[1]] = 0;
-          this.system[y][x+this.alivePos[1]-1] = this.alive[y][x];
+        if (this.system[y+this.alivePos[0]-1][x+this.alivePos[1]-1] === this.alive[y][x]) {
+          this.system[y+this.alivePos[0]-1][x+this.alivePos[1]-1] = 0;
+          this.system[y+this.alivePos[0]-1][x+this.alivePos[1]-2] = this.alive[y][x];
         }
       }
     }
+    this.alivePos[1]--;
+    // console.log("end alivePos= ",this.alivePos);
+    // console.table(this.system);
     return this.alivePos;
   }
 
   Game.prototype.moveRight = function() {
-    this.alivePos[1]++;
-    for (var y = 0; y < this.alive[0].length; y++) {
-      for (var x = 0; x < this.alive[0].length; x++) {
-        if (this.system[y][x+this.alivePos[1]-2] === this.alive[y][x]) {
-          debugger;
-
-          // this.system[y][x+this.alivePos[1]] = 0;
-          // this.system[y][x+this.alivePos[1]] = this.alive[y][x];
+    if (this.alive[0].length === 4) {
+      var rightoffset = 0;
+    } else {
+      var rightoffset = 0;
+    }
+    console.log("start alivePos= ",this.alivePos);
+    for (var y = this.alive[0].length-1; y > 0; y--) {
+      for (var x = this.alive[0].length-1; x > 0; x--) {
+        if (this.system[y+this.alivePos[0]-1][x+this.alivePos[1]-1] === this.alive[y][x]) {
+          this.system[y+this.alivePos[0]-1][x+this.alivePos[1]-1] = 0;
+          this.system[y+this.alivePos[0]-1][x+this.alivePos[1]] = this.alive[y][x];
+          console.log("end alivePos= ",this.alivePos);
+          console.table(this.system);
+          // debugger;
         }
       }
     }
-    console.log("after alivePos: ",this.alivePos);
-    console.table(this.system);
+    this.alivePos[1]++;
     return this.alivePos;
   }
 
@@ -295,39 +325,39 @@ Game.prototype.rotateCCW = function() {
     //play the "move" sound here;
   }
 
-  Game.prototype.updateGrid = function() {
+  Game.prototype.updateGrid = function() { //This function should be moved outside of the game object
     for (let y = 0; y < this.system.length; y++) {
       for (let x = 0; x < this.system[0].length; x++) {
-        switch (this.system[y][x]) {
+        switch (Math.abs(this.system[y][x])) {
           case 0:
             $('#gee'+ y + "-" + x).removeClass().addClass("grid-item");
           break;
           case 1:
-            $('#gee'+ y + "-" + x).addClass("grid-color-brown");
+            $('#gee'+ y + "-" + x).removeClass().addClass("grid-item grid-color-brown");
           break;
           case 2:
-            $('#gee'+ y + "-" + x).addClass("grid-color-red");
+            $('#gee'+ y + "-" + x).removeClass().addClass("grid-item grid-color-red");
           break;
           case 3:
-            $('#gee'+ y + "-" + x).addClass("grid-color-orange");
+            $('#gee'+ y + "-" + x).removeClass().addClass("grid-item grid-color-orange");
           break;
           case 4:
-            $('#gee'+ y + "-" + x).addClass("grid-color-yellow");
+            $('#gee'+ y + "-" + x).removeClass().addClass("grid-item grid-color-yellow");
           break;
           case 5:
-            $('#gee'+ y + "-" + x).addClass("grid-color-green");
+            $('#gee'+ y + "-" + x).removeClass().addClass("grid-item grid-color-green");
           break;
           case 6:
-            $('#gee'+ y + "-" + x).addClass("grid-color-blue");
+            $('#gee'+ y + "-" + x).removeClass().addClass("grid-item grid-color-blue");
           break;
           case 7:
-            $('#gee'+ y + "-" + x).addClass("grid-color-indigo");
+            $('#gee'+ y + "-" + x).removeClass().addClass("grid-item grid-color-indigo");
           break;
           case 8:
-            $('#gee'+ y + "-" + x).addClass("grid-color-purple");
+            $('#gee'+ y + "-" + x).removeClass().addClass("grid-item grid-color-purple");
           break;
           default:
-            $('#gee'+ y + "-" + x).addClass("grid-color-dark");
+            $('#gee'+ y + "-" + x).removeClass().addClass("grid-item grid-color-dark");
           }
         }
       }
@@ -391,7 +421,11 @@ $(document).keydown(function(e) {
     console.log("z key");
     game.insertNewpiece(game.zee);
     break;
-    //G should be used to force "gravity"
+    case 75: //z
+    console.log("k key");
+    game.insertNewpiece(game.one);
+    break;
+    //G should be used to force "gravity, but gravity is just a down key"
     case 71: //g
     console.log("g key");
     break;
@@ -406,6 +440,7 @@ $(document).keydown(function(e) {
 // $(document).ready(function(){
 //   var timerInterval = 1000;
 //   setInterval(function(){
+//    //Add code here for gravity
 //     game.updateGrid();
 //   }, timerInterval);
 // });
